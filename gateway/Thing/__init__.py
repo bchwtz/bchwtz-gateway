6b2a2a20ebe4061880233aae544b65974070a532
sebastiankrause <sebatian.krause94@t-online.de>
2021-10-05 19:00:57 +0200

    added rudementary wrapper class for the paho-functions

---
+++ gateway/Thing/__init__.py
@@ -0,0 +1,67 @@
+import paho.mqtt.client as mqtt
+
+
+class Thing:
+    """
+    A class that represents a thing from mqtt.
+
+    The is a wrapper for certain functions of the phao.mqtt module regarding things.
+    A thing in mqtt can be a sensor for example. This sensor need to publish its data
+    to a broker.
+    This class aims to make it easy create a new client, and publish messages to the
+    broker.
+
+    Methods
+    connect_to_broker(address)
+    set_username_userpassword(username, password)
+    pub_to_channel(topic, payload)
+    disconnect_from_broker()
+    reset_to_factory()
+
+    """
+
+    def __init__(self, username, password):
+        """Creates a new phao-client."""
+        # self.thing_id = thing_id
+        # self.thing_key = thing_key
+        self.client = mqtt.Client(client_id='', clean_session=True, userdata=None, transport='tcp')
+        self.client.username_pw_set(username=username, password=password)
+        # self.client.on_connect = on_connect
+
+    def connect_to_broker(self, address: str):
+        """Wrapper for the paho connect method.
+
+        Connects the thing to the broker. Then calls the phao loop_start-method.
+
+        Args
+        address: str
+            Hostname or ip of the broker
+        """
+        self.client.connect(address)
+        self.client.loop_start()
+
+    def pub_to_channel(self, topic: str, payload: str):
+        """Wrapper for the paho publish method.
+
+        Causes the message to be published to a broker and all from there to be send to all devices subscribed to it.
+
+        Args
+        topic: str
+            The topic/channel the message should be published to
+        payload: str
+            The contents of the message
+        """
+        return self.client.publish(topic=topic, payload=payload)
+
+    def disconnect_from_broker(self):
+        """Wrapper for the phao disconnect method.
+
+        Disconnects the client/Thing from the broker.
+        """
+        self.client.disconnect()
+
+    def reset_to_factory(self):
+        """Wrapper for the paho reinitialise method
+
+        resets the client/Thing to its starting state i.e. as if it was freshly created."""
+        self.client.reinitialise()
