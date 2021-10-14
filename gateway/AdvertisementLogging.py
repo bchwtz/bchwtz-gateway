d853a152e8f81514ee423814d6cc9a0b87f60607
fahol28 <73699786+fahol28@users.noreply.github.com>
2021-10-14 14:09:53 +0200

    Update AdvertisementLogging.py
    Rollback to pygatt for advertisement logging

--- gateway/AdvertisementLogging.py
+++ gateway/AdvertisementLogging.py
@@ -1,83 +1,53 @@
+from ruuvitag_sensor.adapters.nix_hci import BleCommunicationNix
+from ruuvitag_sensor.decoder import get_decoder
+from ruuvitag_sensor.data_formats import DataFormats
 import datetime
-from gateway.AdvertisementDecoder import _get_data_format_5
-import gateway.AdvertisementDecoder
-from bleak import BleakClient
-from functools import partial
-from bleak import BleakScanner
-import nest_asyncio
-import asyncio
+import time
+from gateway import MessageObjects
+
+ble = BleCommunicationNix()
+
+def advertisement_logging():
+            return_value_object=MessageObjects.return_values_from_sensor()
+            last_measurement_number = {}
+
+            try:
+                for ble_data in ble.get_datas():
+
+                    current_time=time.time()
+                    mac = ble_data[0]
+                    data = ble_data[1]
+
+                    (data_format, data) = DataFormats.convert_data(ble_data[1])
+                    if data is not None:
+                        decoded = get_decoder(data).decode_data(data)
+                        if decoded is not None:
+                            del decoded["mac"]
+                            if mac in last_measurement_number:
+                                if decoded["measurement_sequence_number"] != last_measurement_number[mac]:
+                                    last_measurement_number[mac] = decoded["measurement_sequence_number"]
+                                else:
+                                    continue
+                            else:
+                                last_measurement_number[mac]=decoded["measurement_sequence_number"]
+                            msg_obj = return_value_object.from_get_advertisementdata(decoded, mac,
+                                                                                     current_time).returnValue
+                            """
+                             Add push to mainflux here. Use msg_obj from above
+                            """
+
+                            print([mac,current_time,decoded])
+                            keyList = list(decoded.keys())                       
+                            valueList = list(decoded.values())                     
+                            s = "".join([str(x) + "," for x in valueList])                      
+                            date = datetime.date.today()
+                            with open("advertisment-{}.csv".format(date), 'a') as f:
+                                f.write("{}{},{}".format(s, mac, current_time))
+                                f.write("\n")
 
 
-#Channel where advertisements are collected
-UART_RX = '6E400003-B5A3-F393-E0A9-E50E24DCCA9E'
 
-
-
-
-
-mac = []
-adv_data=[]
-nest_asyncio.apply()
-my_loop = asyncio.get_event_loop()
-
-class Event_ts(asyncio.Event):
-    def clear(self):
-        self._loop.call_soon_threadsafe(super().clear)
-
-    def set(self):
-        self._loop.call_soon_threadsafe(super().set)
-
-
-stopEvent = Event_ts()
-
-
-def detection_callback(client: BleakClient, sender: int,data:bytearray):
-
-    if _get_data_format_5(data):
-        #get decoder and parse data
-        decoder = gateway.AdvertisementDecoder.get_decoder(5)
-        adv_data=decoder.decode_data(data)
-        adv_data["mac"]=client.address
-        date = datetime.date.today()
-        current_time = datetime.datetime.now()
-        with open("advertisment-{}.csv".format(date), 'a') as f:
-            f.write("{},{}".format(adv_data,current_time.isoformat()))
-            f.write("\n")
-        print(adv_data)
-
-
-def validate_mac(devices):
-    print("validate")
-    for i in devices:
-        # self.logger.info('Device: %s with Address %s found!' % (i.name, i.address))
-        if ("Ruuvi" in i.name) & (i.address not in mac):
-            print("found")
-            mac.append(i.address)
-            print(mac)
-def start_advertisement_logging():
-    my_loop.run_until_complete(advertisement_logging())
-
-def end__advertisement_logging():
-    stopEvent.set()
-    
-async def advertisement_logging():
-    try:
-        #find devices
-        devices = await BleakScanner.discover(timeout=5.0)
-        validate_mac(devices)
-
-        if len(mac) > 0:
-            for i in mac:
-                async with BleakClient(i) as client:
-                    print(client.address)
-                    #start notify advertisements
-                    await client.start_notify(UART_RX,partial(detection_callback, client) )
-            await stopEvent.wait()
-    except Exception as e:
-        print("Error: {}".format(e))
-        client.stop_notify(UART_RX)
-    # When Ctrl+C is pressed execution of the while loop is stopped
-    except KeyboardInterrupt:
-        client.stop_notify()
-        stopEvent.set()
-        print('Exit')
+            except KeyboardInterrupt:
+            #     # When Ctrl+C is pressed execution of the while loop is stopped
+            #     stopEvent.set()
+                print('Exit')
