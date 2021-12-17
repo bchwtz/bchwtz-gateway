1ca85a43ae491926b9761feab00815c096f2100b
Jippiyay <82330184+Jippiyay@users.noreply.github.com>
2021-12-17 13:28:29 +0100

    correct "any key" to "enter"

--- demos/demo_accelerometer_logging.py
+++ demos/demo_accelerometer_logging.py
@@ -21,12 +21,12 @@ time.sleep(5)
 sensor1.get_logging_status()
 time.sleep(5)
 
-input("Press any key to continue...")
+input("Press enter to continue...")
 
 # Get basic sensor configurations
 sensor1.get_config()
 print(sensor1.sensor_data)
-input("Press any key to continue...")
+input("Press enter to continue...")
 
 #Get acceleration data
 sensor1.get_acceleration_data()
