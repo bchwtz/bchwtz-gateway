355387cdb2cd93a1e1a8211fdd07bb26176c9220
Marius9610 <66902137+Marius9610@users.noreply.github.com>
2022-01-09 19:04:19 +0100

    Append test_import_gateway, test_import_hub, test_import_hubclass.

--- tests/test_gateway.py
+++ tests/test_gateway.py
@@ -1,7 +1,21 @@
 import pytest
 
 def test_import_gateway():
-    import gateway
+    try:
+        import gateway
+    except ImportError:
+        raise ImportError("""Can't import gateway library""")
 
-def test_debug_test():
-    assert(2==2)
+def test_import_hub():
+    try:
+        from gateway import hub
+    except ModuleNotFoundError:
+        raise ModuleNotFoundError("""Can't import gateway library""")
+
+def test_import_hubclass():
+    from gateway import hub
+    myhub = hub.hub()
+    myhub.discover()
+
+#def test_debug_test():
+#    assert(2==2)
