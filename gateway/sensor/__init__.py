850e5f09d813178b39d7f86bb374161bf796642c
Jippijay <giesecke.johanna@fh-swf.de>
2022-02-12 20:42:19 +0100

    added docstrings

--- gateway/sensor/__init__.py
+++ gateway/sensor/__init__.py
@@ -50,24 +50,24 @@ class Event_ts(asyncio.Event):
 
 class SensorConfig():
     def __init__(self, sample_rate=None, resolution=None, scale=None, dsp_function=None, dsp_parameter=None, mode=None, divider=None, mac=None):
-        """Storage dto for the sensor's config. It is used to store all important parameters.
+        """Storage dto for the sensor's config. It is used to store all important parameters. This config object shall be updated on each update to the sensor config itself.
 
-        :param sample_rate: [description], defaults to None
-        :type sample_rate: [type], optional
-        :param resolution: [description], defaults to None
-        :type resolution: [type], optional
-        :param scale: [description], defaults to None
-        :type scale: [type], optional
-        :param dsp_function: [description], defaults to None
-        :type dsp_function: [type], optional
-        :param dsp_parameter: [description], defaults to None
-        :type dsp_parameter: [type], optional
-        :param mode: [description], defaults to None
-        :type mode: [type], optional
-        :param divider: [description], defaults to None
-        :type divider: [type], optional
-        :param mac: [description], defaults to None
-        :type mac: [type], optional
+        :param sample_rate: current samplerate of the sensor, defaults to None
+        :type sample_rate: int, optional
+        :param resolution: current resolution of the sensor, defaults to None
+        :type resolution: int, optional
+        :param scale: current scale of the sensor, defaults to None
+        :type scale: int, optional
+        :param dsp_function: current dsp function, defaults to None
+        :type dsp_function: int, optional
+        :param dsp_parameter: current dsp parameter, defaults to None
+        :type dsp_parameter: int, optional
+        :param mode: current mode of the sensor, defaults to None
+        :type mode: int, optional
+        :param divider: current divider of the sensor, defaults to None
+        :type divider: int, optional
+        :param mac: mac address of the sensor, defaults to None
+        :type mac: string, optional
         """
         self.sample_rate = sample_rate
         self.resolution = resolution
