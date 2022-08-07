f192197d81323046deaab7f80befbeaa40c81e56
Sebastian Krause <sebastian.krause@fh-swf.de>
2022-08-07 12:11:17 +0100

    now ignoring pytest_asyncio deprecation warning.

--- gatewayn/tests/hub_tests.py
+++ gatewayn/tests/hub_tests.py
@@ -1,6 +1,9 @@
 import pytest
 from gatewayn.hub.hub import Hub
 
+import warnings
+# https://github.com/pytest-dev/pytest-asyncio/issues/212
+warnings.filterwarnings("ignore", category=DeprecationWarning) 
 test_hub = Hub()
 
 def test_get_tag_by_mac():
