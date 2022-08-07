90d75442aace6f79dcd88811a79efd060396809f
Sebastian Krause <sebastian.krause@fh-swf.de>
2022-08-07 12:39:26 +0100

    renamed the file to clearer indicate its purpose.

--- gatewayn/tests/test_hub_no_tags.py
+++ gatewayn/tests/test_hub_no_tags.py
@@ -10,10 +10,16 @@ class TestHub:
 
     def test_get_tag_by_mac(self):
         assert self.test_hub.get_tag_by_mac() is None
+    
+    def test_get_tag_by_name_wrong_Mac(self):
+        assert self.test_hub.get_tag_by_mac(mac="not_existing") is None    
 
     def test_get_tag_by_name(self):
         assert self.test_hub.get_tag_by_name() is None
 
     def test_get_tag_by_name_wrong_name(self):
         assert self.test_hub.get_tag_by_name(name="not_existing") is None
+
+    def test_no_tags_found(self):
+        assert len(self.test_hub.tags) == 0
 
