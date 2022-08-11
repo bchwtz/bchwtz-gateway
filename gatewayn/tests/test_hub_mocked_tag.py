b00affbb95e45a27639f1748ce6311015dbd184c
Sebastian Krause <sebastian.krause@fh-swf.de>
2022-08-11 17:47:41 +0100

    added the first two tests.

--- gatewayn/tests/test_hub_mocked_tag.py
+++ gatewayn/tests/test_hub_mocked_tag.py
@@ -14,9 +14,12 @@ class TestHub():
         address='6C:5D:7F:8G:9H',
         name='TestDevice')
 
-    test_tag = TagBuilder().from_device(ble_device)
+    test_tag = TagBuilder().from_device(ble_device).build()
     test_hub = Hub()
     test_hub.tags.append(test_tag)
 
     def test_get_tag_by_name_wrong_Mac(self):
-        assert self.test_hub.get_tag_by_mac(mac="6C:5D:7F:8G:9H") is None 
+        assert isinstance(self.test_hub.get_tag_by_mac(mac="6C:5D:7F:8G:9H"),Tag)
+    
+    def test_get_tag_by_name_wrong_name(self):
+        assert isinstance(self.test_hub.get_tag_by_name(name="TestDevice"),Tag)
