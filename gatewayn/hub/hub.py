e23cd02d1e70c23ddd9516cdb43a7635aee4efbe
caos <sysexect@gmail.com>
2022-08-22 23:12:30 +0200

    added persistent tags in hub, added gateway basic impl

--- gatewayn/hub/hub.py
+++ gatewayn/hub/hub.py
@@ -1,4 +1,6 @@
 import asyncio
+import logging
+import time
 from gatewayn.tag.tag import Tag
 from gatewayn.tag.tag_builder import TagBuilder
 from gatewayn.drivers.bluetooth.ble_conn.ble_conn import BLEConn
@@ -10,10 +12,18 @@ class Hub():
         self.main_loop = asyncio.get_event_loop()
         self.tags: list[Tag] = []
         self.ble_conn = BLEConn()
+        self.logger = logging.getLogger("Hub")
+        self.logger.setLevel(logging.DEBUG)
 
-    def discover_tags(self, timeout: float = 5.0) -> None:
-        self.tags = []
+    def discover_tags(self, timeout: float = 5.0, rediscover: bool = False) -> None:
         devices = self.main_loop.run_until_complete(self.ble_conn.scan_tags(Config.GlobalConfig.bluetooth_manufacturer_id.value, timeout))
+        if not rediscover:
+            self.__check_tags_online_state(devices)
+            if not self.__has_new_devices(devices):
+                self.logger.debug("found no new devices")
+                return
+            devices = filter(lambda dev: not any(dev.address == t.address for t in self.tags), devices)
+        self.logger.debug("found new devices")
         self.__devices_to_tags(devices)
     
     def get_tag_by_mac(self, mac: str = None) -> Tag:
@@ -47,3 +57,20 @@ class Hub():
     def __devices_to_tags(self, devices: list[BLEDevice]) -> list[Tag]:
         self.tags = [TagBuilder().from_device(dev).build() for dev in devices]
         return self.tags
+
+    def __has_new_devices(self, devices: list[BLEDevice]) -> bool:
+        for device in devices:
+            if not any(t.address == device.address for t in self.tags):
+                return True
+        return False
+
+    def __check_tags_online_state(self, devices: list[BLEDevice]) -> None:
+        for tag in self.tags:
+            self.logger.debug(tag.__dict__)
+            if not any(tag.address == dev.address for dev in devices):
+                tag.online = False
+                self.logger.debug(f"setting tag offline: {tag.address}")
+            else:
+                tag.online = True
+                tag.last_seen = time.time()
+                self.logger.debug(f"setting tag online: {tag.address}")
--- gatewayn/tag/tag_interface/encoder.py
+++ gatewayn/tag/tag_interface/encoder.py
@@ -15,24 +15,26 @@ class Encoder():
     def encode_config(self, config: TagConfig) -> str:
         if config.samplerate not in Config.AllowedValues.samplerate.value:
             config.samplerate = 0xff
+        if config.samplerate >= 255:
+            config.samplerate = 0xc9
         sampleratestr = "{:02x}".format(config.samplerate)
-        print(sampleratestr)
         if config.resolution not in Config.AllowedValues.sample_resolution.value:
             config.resolution = 0xff
         resolutionstr = "{:02x}".format(config.resolution)
         if config.scale not in Config.AllowedValues.scale.value:
             config.scale = 0xff
         scalestr = "{:02x}".format(config.scale)
+        # TODO : make another class that checks the combination of divider and samplerate and calculates a valid config
         if config.divider > 254:
             self.logger.warn("divider overflowed (max 0xff), resetting to 0xff")
             config.divider = 0xff
         dividerstr = "{:02x}".format(config.divider)
         command = Config.Commands.set_tag_config_substr.value + sampleratestr + resolutionstr + scalestr + "ffffff" + dividerstr + "00"
-        self.logger.info("Set sensor config {}".format(command))
+        self.logger.debug("Set sensor config {}".format(command))
         return command
 
     def encode_time(self, time: float = None) -> str:
         now = struct.pack("<Q", int(time * 1000)).hex()
         command = Config.Commands.set_tag_time_substr.value + now
-        self.logger.info("Set sensor time {}".format(command))
+        self.logger.debug("Set sensor time {}".format(command))
         return command
