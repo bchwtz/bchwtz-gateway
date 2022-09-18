a3d93bf1007f5dc80efea2276b5b4def8eedea61
caos <sysexect@gmail.com>
2022-09-18 19:32:38 +0200

    added gateway, added filter to make upserts on mongo possible

--- db_connector/cmd/main.go
+++ db_connector/cmd/main.go
@@ -1,76 +1,13 @@
 package main
 
 import (
-	"encoding/json"
-	"os"
-
-	"github.com/bchwtz-fhswf/gateway/db_connector/internal/model"
-	"github.com/joho/godotenv"
+	"github.com/bchwtz-fhswf/gateway/db_connector/internal/gateway"
 	"github.com/sirupsen/logrus"
-	"github.com/systematiccaos/going-forward/db"
-	"github.com/systematiccaos/going-forward/mqtt"
 	"github.com/systematiccaos/going-forward/util"
 )
 
 func main() {
 	util.SetupLogs()
-	gw := Gateway{}
-	gw.run()
-}
-
-type Gateway struct {
-	mqclient                  mqtt.Client
-	hub                       model.Hub
-	get_advertisement_channel chan mqtt.MQTTSubscriptionMessage
-	database                  *db.Database
-}
-
-func (gw *Gateway) run() {
-	gw.mqclient = mqtt.Client{}
-	if err := godotenv.Load("../.env"); err != nil {
-		logrus.Errorln(err)
-	}
-	gw.ConnectMongo()
-	donech := make(chan bool)
-	gw.mqclient.Connect(os.Getenv("MQTT_BROKER")+":"+os.Getenv("MQTT_PORT"), os.Getenv("MQTT_CLIENTID"), os.Getenv("MQTT_USER"), os.Getenv("MQTT_PASSWORD"), true)
-	gw.get_advertisement_channel = make(chan mqtt.MQTTSubscriptionMessage)
-	gw.hub = model.Hub{}
-	gw.listenAdvertisements()
-	go gw.writeAdvertisementsToDB()
-	<-donech
-}
-
-func (gw *Gateway) listenAdvertisements() {
-	topic := os.Getenv("TOPIC_LISTEN_ADV")
-
-	if err := gw.mqclient.Subscribe(topic, gw.get_advertisement_channel); err != nil {
-		logrus.Fatalln(err)
-	}
-	logrus.Infoln("subscribing on channel " + topic)
-}
-
-func (gw *Gateway) writeAdvertisementsToDB() {
-	for {
-		adv, more := <-gw.get_advertisement_channel
-		if !more {
-			return
-		}
-		msgbt := adv.Message.Payload()
-		logrus.Traceln(string(msgbt))
-		if err := json.Unmarshal(msgbt, &gw.hub); err != nil {
-			logrus.Fatalln(err)
-			continue
-		}
-		logrus.Traceln(gw.hub.Tags)
-		gw.database.Save(&gw.hub.Tags)
-		adv.Message.Ack()
-	}
-}
-
-func (gw *Gateway) ConnectMongo() {
-	db, err := db.ConnectMongo()
-	if err != nil {
-		logrus.Fatalln(err)
-	}
-	gw.database = db
+	gw := gateway.NewGateway()
+	logrus.Println(gw)
 }
