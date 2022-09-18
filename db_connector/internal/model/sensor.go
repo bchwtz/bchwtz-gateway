5e364306ca1bd4bd670b7fd3d5f957d39e31bd0e
caos <sysexect@gmail.com>
2022-09-18 19:57:43 +0200

    fitted interface to new sensor struct

--- db_connector/internal/model/sensor.go
+++ db_connector/internal/model/sensor.go
@@ -2,14 +2,12 @@ package model
 
 type Sensor interface {
 	GetName() string
-	GetLastMeasurement() interface{}
 	GetMeasurements() []interface{}
 }
 
 type BasicSensor struct {
-	Name            string        `json:"name"`
-	LastMeasurement interface{}   `json:"last_measurement"`
-	Measurements    []interface{} `json:"measurements"`
+	Name         string        `json:"name"`
+	Measurements []interface{} `json:"measurements"`
 }
 
 func (s *BasicSensor) GetName() string        { return s.Name }
@@ -19,13 +17,6 @@ func (s *TemperatureSensor) GetName() string  { return s.Name }
 func (s *HumiditySensor) GetName() string     { return s.Name }
 func (s *BatterySensor) GetName() string      { return s.Name }
 
-func (s *BasicSensor) GetLastMeasurement() interface{}        { return s.LastMeasurement }
-func (s *AccelerationSensor) GetLastMeasurement() interface{} { return s.LastMeasurement }
-func (s *BarometerSensor) GetLastMeasurement() interface{}    { return s.LastMeasurement }
-func (s *TemperatureSensor) GetLastMeasurement() interface{}  { return s.LastMeasurement }
-func (s *HumiditySensor) GetLastMeasurement() interface{}     { return s.LastMeasurement }
-func (s *BatterySensor) GetLastMeasurement() interface{}      { return s.LastMeasurement }
-
 func (s *BasicSensor) GetMeasurements() []interface{}        { return s.Measurements }
 func (s *AccelerationSensor) GetMeasurements() []interface{} { return s.Measurements }
 func (s *BarometerSensor) GetMeasurements() []interface{}    { return s.Measurements }
@@ -33,11 +24,8 @@ func (s *TemperatureSensor) GetMeasurements() []interface{}  { return s.Measurem
 func (s *HumiditySensor) GetMeasurements() []interface{}     { return s.Measurements }
 func (s *BatterySensor) GetMeasurements() []interface{}      { return s.Measurements }
 
-func (s *AccelerationSensor) GetMovementCounter() int { return s.MovementCounter }
-
 type AccelerationSensor struct {
 	BasicSensor
-	MovementCounter int `json:"movement_counter"`
 }
 
 type BarometerSensor struct {
