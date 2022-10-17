0f4830a55be6b8d2cf954d984e795b821e094562
caos <sysexect@gmail.com>
2022-10-17 16:39:34 +0200

    added new env params

--- run_on_pi.sh
+++ run_on_pi.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+# This script is intended to run on a raspberry pi to start the gateway microservices cluster - everything is going to run inside docker
 cd deployments/gateway
 docker-compose --env-file ../../.env -f docker-compose.yml -f docker-compose.rpi.yml up -d
