3bd56ccf22c65668ae3cac0a7203a6bb36b4bb8a
caos <sysexect@gmail.com>
2022-10-27 11:51:35 +0200

    added dev runscript

---
+++ run_dev.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# This script is intended to run on a raspberry pi to start the gateway microservices cluster - everything is going to run inside docker
+if [ ! -f .env ]
+then
+    echo "env did not exist, will copy sample values!"
+    cp .env-default .env
+fi
+cd deployments/gateway
+docker-compose --env-file ../../.env -f docker-compose.yml up -d
+cd -
+echo "waiting for docker service startup..."
+sleep 20
+python3 demos/demo_gateway.py
