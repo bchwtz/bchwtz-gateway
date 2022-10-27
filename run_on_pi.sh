71c92d4fb2dd1775f43f1e6c7a3f215685fb58b3
caos <sysexect@gmail.com>
2022-10-27 11:45:11 +0200

    archived old data

--- run_on_pi.sh
+++ run_on_pi.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 # This script is intended to run on a raspberry pi to start the gateway microservices cluster - everything is going to run inside docker
+if [ ! -f /path/to/file ]
+then
+    echo "env did not exist, will copy sample values!"
+    cp .env-default .env
+fi
 cd deployments/gateway
 docker-compose --env-file ../../.env -f docker-compose.yml -f docker-compose.rpi.yml up -d
