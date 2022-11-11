4c4fd8ee0b414dcc7a1f757a0c77a7700073ee6f
Sebastian Krause <krause.sebastian@fh-swf.de>
2022-11-11 12:33:00 +0000

    started get_acceleration_data.py

--- demos/current_time_demo.py
+++ demos/current_time_demo.py
@@ -36,4 +36,4 @@ def get_current_tag_time():
     print(datetime.utcfromtimestamp(td).strftime('%Y-%m-%d %H:%M:%S'))
 
 if __name__ == "__main__":
-    get_current_tag_time
+    get_current_tag_time()
---
+++ demos/get_acceleration_data.py
@@ -0,0 +1,20 @@
+"""This demo shows how to get the currently set hearbeat from the tag."""
+import asyncio
+from gateway.hub.hub import Hub
+
+def get_tag_acceleration_data():
+    """This demo shows how to get the acceleration_log from the tag.
+
+    The first step is always to create a new asyncio eventloop in which the whole communication will be handled.
+    On the mainloop the run_until_complete method gets called and this method gets the hub.discover_tags() callback, with a 5-second timeout as parameter.
+    When all the tags in the area are discovered, one of them get selected by the MAC-adress you provide.
+    """
+    hub = Hub()
+    main_loop = asyncio.get_event_loop()
+    main_loop.run_until_complete(hub.discover_tags(5))
+    my_tag = hub.get_tag_by_address('CA:A5:49:28:D8:87')
+    main_loop.run_until_complete(my_tag.get_flash_statistics())
+    print("HEllo")
+
+if __name__ == '__main__':
+    get_tag_acceleration_data()
