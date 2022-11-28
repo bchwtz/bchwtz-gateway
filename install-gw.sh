b00cfa66768632d15bd09e286cd50bdb2760006e
caos <sysexect@gmail.com>
2022-11-28 22:50:31 +0100

    added conditional patch of mongo

--- install-gw.sh
+++ install-gw.sh
@@ -7,7 +7,7 @@ MONGO_PASSW=$(openssl rand -base64 24 | sed -e 's/\///g')
 MQTT_PASSW=$(openssl rand -base64 24 | sed -e 's/\///g')
 
 if [ -f $BINARY_PATH$BINARY_NAME ]; then
-    echo "executable $BINARY_PATH$BINARY_NAME already exists - aborting installation"
+    echo "Executable $BINARY_PATH$BINARY_NAME already exists - aborting installation"
     exit
 fi
 sudo apt update && sudo apt install -y docker.io docker-compose
@@ -20,19 +20,23 @@ if [ ! -d $DIST_DIR ]; then
     wget https://bchwtz.github.io/bchwtz-gateway/dist/.env-default
     wget https://bchwtz.github.io/bchwtz-gateway/dist/uninstall-gw.sh
     mv docker-compose.yml docker-compose.std.yml
-    sed -i -e 's/image: mongo$/image: apcheamitru\/arm32v7-mongo/' docker-compose.yml
+    IS_32=$(uname -a | grep -i armv7l)
+    if [ ! -z "$IS_32" ] ; then
+        echo "Patching mongo image to comply to 32bits - consider using a 64bit-os!"
+        sed -i -e 's/image: mongo$/image: apcheamitru\/arm32v7-mongo/' docker-compose.yml
+    fi
     chmod +x uninstall-gw.sh
 fi
 if [ -f .env-default ]
 then
-    echo "configuring auto-load of env-variables"
+    echo "Configuring auto-load of env-variables"
     awk '!/^$/{print "export " $0}' .env-default | sed -e 's/\(MQTT_PASSWORD=\).*/\1"'$MQTT_PASSW'"/g' | sed -e 's/\(MONGO_PASSWORD=\).*/\1"'$MONGO_PASSW'"/g' > gateway-vars.sh
     . $(pwd)/gateway-vars.sh
     sudo cp gateway-vars.sh /etc/profile.d/
     sudo mv $(pwd)/gw-arm64 $BINARY_PATH$BINARY_NAME
     mv .env-default .env
 fi
-echo "generating docker-compose"
+echo "Generating docker-compose"
 docker-compose --project-name gateway -f docker-compose.std.yml -f docker-compose.rpi.yml config > docker-compose.yml
 rm docker-compose.std.yml docker-compose.rpi.yml
 docker-compose up
