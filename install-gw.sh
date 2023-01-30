a19df4a53cf65b9a617d802f97c7e870da1bab7a
caos <sysexect@gmail.com>
2023-01-30 22:11:26 +0100

    added updater

--- install-gw.sh
+++ install-gw.sh
@@ -2,7 +2,7 @@
 
 BINARY_PATH=/usr/bin/
 BINARY_NAME=gw
-DIST_DIR=gateway-autoinstall
+DIST_DIR=/opt/gateway-autoinstall
 MONGO_PASSW=$(openssl rand -base64 24 | sed -e 's/\///g')
 MQTT_PASSW=$(openssl rand -base64 24 | sed -e 's/\///g')
 # sudo apt install -y docker.io
@@ -11,7 +11,7 @@ if [ -f $BINARY_PATH$BINARY_NAME ]; then
     exit
 fi
 
-if [ ! -d $DIST_DIR ]; then
+downloadSources() {
     mkdir -p $DIST_DIR
     cd $DIST_DIR
     # curl -fsSL https://get.Docker.com -o get-Docker.sh
@@ -30,28 +30,53 @@ if [ ! -d $DIST_DIR ]; then
         sed -i 's/\/lib\/libreadline\.so/\/lib\/arm-linux-gnueabihf\/libreadline.so.7/g' docker-compose.rpi.yml
     fi
     chmod +x uninstall-gw.sh
-fi
-if [ -f .env-default ]
-then
-    echo "Configuring auto-load of env-variables"
-    sed -i -e 's/\(MQTT_PASSWORD=\).*/\1"'$MQTT_PASSW'"/g' .env-default
-    sed -i -e 's/\(MONGO_PASSWORD=\).*/\1"'$MONGO_PASSW'"/g' .env-default
-    cp .env-default .env-local
-    sed -i -e 's/\(MQTT_BROKER=\).*/\1"localhost"/g' .env-local
-    awk '!/^$/{print "export " $0}' .env-local > gateway-vars.sh
-    echo "INSTALLDIR=$(pwd)" >> gateway-vars.sh
-    cat .env-local | sed -e 's/\(.*\)=.*$/unset \1/g' > unset-gateway-vars.sh
-    echo "unset INSTALLDIR" >> unset-gateway-vars.sh
+}
 
-    sudo cp gateway-vars.sh /etc/profile.d/
-    sudo mv $(pwd)/gw-arm $BINARY_PATH$BINARY_NAME
-    sudo chmod +x $BINARY_PATH$BINARY_NAME
-    chmod +x unset-gateway-vars.sh
-fi
-echo "Generating docker-compose"
+setupEnv() {
+    if [ -f .env-default ]
+    then
+        echo "Configuring auto-load of env-variables"
+        sed -i -e 's/\(MQTT_PASSWORD=\).*/\1"'$MQTT_PASSW'"/g' .env-default
+        sed -i -e 's/\(MONGO_PASSWORD=\).*/\1"'$MONGO_PASSW'"/g' .env-default
+        cp .env-default .env-local
+        sed -i -e 's/\(MQTT_BROKER=\).*/\1"localhost"/g' .env-local
+        awk '!/^$/{print "export " $0}' .env-local > gateway-vars.sh
+        echo "INSTALLDIR=$(pwd)" >> gateway-vars.sh
+        cat .env-local | sed -e 's/\(.*\)=.*$/unset \1/g' > unset-gateway-vars.sh
+        echo "unset INSTALLDIR" >> unset-gateway-vars.sh
+
+        sudo cp gateway-vars.sh /etc/profile.d/
+        sudo mv $(pwd)/gw-arm $BINARY_PATH$BINARY_NAME
+        sudo chmod +x $BINARY_PATH$BINARY_NAME
+        chmod +x unset-gateway-vars.sh
+    else
+        echo "error - could not find required .env-default - please check if it is on your filesystem!"
+        exit
+    fi
+}
+
+run() {
+    echo "Generating docker-compose"
 docker compose --env-file .env-default --project-name gateway -f docker-compose.std.yml -f docker-compose.rpi.yml config > docker-compose.yml
 bash $(pwd)/gateway-vars.sh
 rm docker-compose.std.yml docker-compose.rpi.yml
 docker compose pull
 docker compose up -d
 echo "Congratulations - your gateway is up and running! Please logout and login again, to load the required environment variables!"
+}
+
+if [ ! -d $DIST_DIR ]; then
+    downloadSources
+    setupEnv
+    run
+else
+    echo "Gateway already detected. Do you wish to update all components??"
+    select yn in "Yes" "No"; do
+        case $yn in
+            Yes ) downloadSources;run; break;;
+            No ) exit;;
+        esac
+    done
+    
+fi
+
