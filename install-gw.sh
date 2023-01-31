d9e0e129e927e0b086a15d2fc91f405d7f086777
caos <sysexect@gmail.com>
2023-01-31 01:03:42 +0100

    added prompt

--- install-gw.sh
+++ install-gw.sh
@@ -63,7 +63,7 @@ echo "Congratulations - your gateway is up and running! Please logout and login 
 }
 
 update() {
-    read -p "Gateway already detected. Do you wish to update all components?" -n 1 -r
+    read -p "Gateway already detected. Do you wish to update all components?" -r
     echo
     if [[ $REPLY =~ ^[Yy]$ ]]; then
         downloadSources
