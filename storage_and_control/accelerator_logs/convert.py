5915b1e641323d17ad1eda0e2654ad299870a26e
lucajanas <58073358+lucajanas@users.noreply.github.com>
2023-03-09 22:12:40 +0100

    Update convert.py

--- storage_and_control/accelerator_logs/convert.py
+++ storage_and_control/accelerator_logs/convert.py
@@ -1,27 +1,44 @@
+import sys
 import json
 import pandas as pd
 
-with open("test.json") as f:
+filename = sys.argv[1]
+
+with open(filename) as f:
     data = json.load(f)[0]
 
-    tag_address = data[0]["tag"]["address"]
-    tag_name = data[0]["tag"]["name"]
-    tag_time = data[0]["tag"]["time"]
+    #tag_address = data[0]["tag"]["address"]
+    #tag_name = data[0]["tag"]["name"]
+    #tag_time = data[0]["tag"]["time"]
 
     measurement_list = []
 
     for measurement in data[1:]:
         if "measurement" in measurement:
-            for m in measurement["measurement"]:
-                measurement_list.append(m)
-
-    #measurement_list = [m for m in measurement["measurement"] for measurement in data[1:] if "measurement" in measurement]
-    print(type(measurement_list[0]))
+            if isinstance(measurement["measurement"], dict):
+                measurement_list.append(measurement["measurement"])
+            else:
+                for m in measurement["measurement"]:
+                    measurement_list.append(m)
 
     df = pd.DataFrame(measurement_list)
+
+    df = df.drop(["gathering_type", "measurements", "sequence_number"], axis=1)
+    cols = df.drop("recorded_time", axis=1).columns.to_list()
+
+   # df["tag_address"] = tag_address
+   # df["tag_name"] = tag_name
+
+    df["timestamp"] = pd.to_datetime(df.recorded_time, unit="ms")
+
+    #df = df[["tag_address", "tag_name", "timestamp", "recorded_time"] + cols]
+    df = df[["timestamp", "recorded_time"] + cols]
+
+
+    df = df.sort_values(by=["timestamp"])
+
+    df = df.set_index("timestamp")
+
     df.to_csv("convert.csv")
 
-
-    print(len(data))
-    print(tag_address)
-    print(tag_name)
+    print("Created csv export!")
